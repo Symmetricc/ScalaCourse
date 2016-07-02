22180df883863622a1e363b75b3299b1a7487f1a
Symmetricc <m.saedramadan@gmail.com>
2016-07-02 19:17:47 +0300

    Some changing to the folder

--- countChange.scala
+++
@@ -1,5 +0,0 @@
- def countChange(money: Int, coins: List[Int]): Int = {
-    if(money < 0 || coins.isEmpty) 0
-    else if (money == 0) 1 else countChange(money - coins.head,coins) + countChange(money,coins.tail)
-
-  }
--- pascal.scala
+++
@@ -1,4 +0,0 @@
-def pascal(c: Int, r: Int): Int = {
-    if(r == 0) 1
-    else (if(c-1 >= 0) pascal(c-1,r-1) else 0) + (if(c <= r-1) pascal(c,r-1) else 0)
-  }
